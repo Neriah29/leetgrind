d19cb3b2433a3635a9a224b668902ec2101bedfd
Nerez <neriahakwuokolo@gmail.com>
2026-05-15 22:17:25 -0400

    Add: generate-parentheses - submission-4

---
+++ Data Structures & Algorithms/generate-parentheses/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def dfs(l, r, s):
+            if l == n and r == n:
+                res.append(s)
+                return
+            
+            if l < n:
+                dfs(l+1,r,s+"(")
+            if r < l:
+                dfs(l,r+1,s+")")
+        
+        dfs(0,0,"")
+        return res
