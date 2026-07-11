90abf8fa8fd29eae462d7a361a626cbe46c403b6
Nerez <neriahakwuokolo@gmail.com>
2026-07-11 13:21:21 -0400

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        """
+        left and right pointers 
+        one starts at the beginning the right at the end, oer iteration do sun 
+        if cursum is higher than target, or vicevrsa move pointer in right 
+        direction. 
+        """
+
+        l, r = 0, len(numbers) - 1 
+        while l < r:
+            cursum = numbers[l] + numbers[r]
+
+            if cursum > target:
+                r -= 1
+            elif cursum < target:
+                l += 1
+            else:
+                return [l+1, r+1]
