f652367d7a4f375107121298c263121284b5425f
Nerez <neriahakwuokolo@gmail.com>
2026-07-26 12:05:44 -0400

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """
+        we use a maxHeap for each active window.
+        on each iteration, you pop the max. If the index of the max is less than 
+        l, keep popping and dont add to res list. 
+        """
+
+        #what do we need initialized?
+        maxHeap = []
+        l = 0
+        res= []
+        nums = [[-num, idx] for idx, num in enumerate(nums)]
+
+        for r in range(len(nums)):
+            heapq.heappush(maxHeap, nums[r])
+            if r - l + 1 < k:
+                continue
+            else:
+                while maxHeap[0][1] < l:
+                    heapq.heappop(maxHeap)
+                res.append(-maxHeap[0][0])
+        
+        return res
+
