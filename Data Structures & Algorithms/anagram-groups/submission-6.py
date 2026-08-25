44bced76eef66b298f1b923b9a95715970044f64
Nerez <neriahakwuokolo@gmail.com>
2026-08-24 21:54:52 -0500

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,28 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        solution i am thinking of seems to be O(n * w)
+        where n = len(strs) and w = max length of a word
+
+        iterate over strs and make a counter dict, possibly convert that to a tuple?
+        the tuple will point to the index of the list in the res
+        """
+        res = []
+        checkMap = {}
+
+        for idx in range(len(strs)):
+            #for each we get the tuple containing the frequencies
+            curCountTuple = tuple(sorted(strs[idx]))
+
+            #check if the tuple is alr in the map
+            #if it is already in, then go to the index of res and append
+            if checkMap and curCountTuple in checkMap:
+                resIndex = checkMap[curCountTuple]
+                res[resIndex].append(strs[idx])
+            else:
+                res.append([strs[idx]])
+                checkMap[curCountTuple] = len(res) - 1
+            
+        return res
+
+
