b24c3d90b5b4530081adbe121497b52946cc9720
Nerez <neriahakwuokolo@gmail.com>
2026-09-02 21:42:00 -0500

    Add: sliding-window-maximum - submission-5

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        """
+        we will have an array containing potential maxes, using indexes.
+        as r moves, we will increment but if 
+        """
+        res = []
+        maxWindow = deque([])
+        l = 0
+        for r in range(len(nums)):
+            while maxWindow and nums[maxWindow[0]] < nums[r]:
+                maxWindow.pop()
+            
+            maxWindow.append(r)
+
+            if r - l + 1 < k:
+                continue
+            res.append(nums[maxWindow[0]])
+
+            if maxWindow[0] == l:
+                maxWindow.popleft()
+            l += 1
+
+        
+        return res
