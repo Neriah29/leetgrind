acf39266a00db331b216af58e70304bd8ef859c9
Nerez <neriahakwuokolo@gmail.com>
2026-09-23 23:36:45 -0500

    Add: last-stone-weight - submission-10

---
+++ Data Structures & Algorithms/last-stone-weight/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        """
+        pop top two, break, push, check conditions, return
+        """
+        stones = [-weight for weight in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            #on every iteration, we want to pop the largest two, and smash them, then push back to the heap
+            #if anything remains
+            stone1, stone2 = heapq.heappop(stones), heapq.heappop(stones)
+            rem = -stone1 --stone2
+            if rem: heapq.heappush(stones, -rem)
+        
+
+        return -stones[0] if stones else 0
